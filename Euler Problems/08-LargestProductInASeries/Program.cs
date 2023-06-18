2f34f746639c2db2a92d12fbca42df4a8cd2dba3
Marijn459595 <78910777+Marijn459595@users.noreply.github.com>
2023-06-18 16:39:14 +0200

    Added euler problem 8

---
+++ Euler Problems/08-LargestProductInASeries/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculations Calculations = new Calculations();
+
+            Calculations.FindProduct();
+
+            Console.ReadKey();
+        }
+    }
+}
